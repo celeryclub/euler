50eabb104334a3a690f1e65c7fc996f38a494d8d
Stephen Davis <stephendavis89@gmail.com>
2013-07-15 20:57:04 -0400

    refactored problem 12/Haskell

--- Haskell/12.hs
+++ Haskell/12.hs
@@ -1,16 +1,30 @@
 -- http://projecteuler.net/problem=12
 
+-- Refactoring help:
+-- http://forum.projecteuler.net/viewtopic.php?f=50&t=2457&hilit=012&start=15
+-- http://www.haskell.org/haskellwiki/99_questions/Solutions/35
+
+import Data.List (group, foldl1')  
+
 firstTriangleNumberWithOverNDivisors :: Integer -> Integer
-firstTriangleNumberWithOverNDivisors n = checkTriangleNumber n 0 1
-
-checkTriangleNumber :: Integer -> Integer -> Integer -> Integer
-checkTriangleNumber requiredDevisors current thisAddend =
-  let triangle = current + thisAddend
-  in if (length (factorize triangle)) > fromIntegral requiredDevisors
-     then triangle
-     else checkTriangleNumber requiredDevisors triangle $ thisAddend + 1
-
-factorize n = [x | x <- [1..n], n `mod` x == 0]
+firstTriangleNumberWithOverNDivisors n =
+  checkTriangleNumbers n 1 2
+  where
+    checkTriangleNumbers :: Integral a => a -> Integer -> Integer -> Integer
+    checkTriangleNumbers requiredDevisors current thisAddend =
+      let triangle = current + thisAddend
+      in if (numberOfFactors triangle) > fromIntegral requiredDevisors
+            then triangle
+            else checkTriangleNumbers requiredDevisors triangle $ thisAddend + 1
+          where
+            numberOfFactors :: Integer -> Int
+            numberOfFactors n =
+              foldl1' (*) $ map (+1) $ map length $ group $ factor n
+              where
+                factor :: Integer -> [Integer]
+                factor 1 = []
+                factor n = let prime = head $ dropWhile ((/= 0) . mod n) [2..n]
+                           in (prime :) $ factor $ div n prime
 
 main :: IO ()
 main = print $ firstTriangleNumberWithOverNDivisors 500

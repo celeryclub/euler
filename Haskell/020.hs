61cdb329414f351395e79b7967a72f2c7411f4b3
Stephen Davis <stephendavis89@gmail.com>
2013-07-16 12:24:49 -0400

    refactored 20/Haskell for clarity

--- Haskell/020.hs
+++ Haskell/020.hs
@@ -1,18 +1,13 @@
 -- http://projecteuler.net/problem=20
 
+import Data.List (foldl1')
 import Data.Char (digitToInt)
 
 sumOfFactorialDigits :: (Integral a, Show a) => a -> Int
 sumOfFactorialDigits n = sum $ map digitToInt $ show $ factorial n
   where
     factorial :: Integral a => a -> a
-    factorial n = accumSum 1 n
-      where
-        accumSum :: Integral a => a -> a -> a
-        accumSum total current =
-            if current > 0
-              then accumSum (total * current) $ current - 1
-              else total
+    factorial n = foldl1' (*) [1 .. n]
 
 main :: IO ()
 main = print $ sumOfFactorialDigits 100

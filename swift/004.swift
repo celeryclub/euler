66478b42dd3950115022f490ffbd4c2155333a41
Stephen Davis <stephendavis89@gmail.com>
2014-10-31 17:40:13 -0400

    Refactor problem 4 in Swift for better performance

--- swift/004.swift
+++ swift/004.swift
@@ -8,15 +8,25 @@ func largestPalindromeProduct(digits: Int) -> Int {
   var largestProduct = 0
 
   for i in reverse(smallestFactor...largestFactor) {
+    // If i is less than the square root of the largest product, we have already
+    // found the largest product
+    if i * i < largestProduct {
+      break
+    }
+
     for j in reverse(smallestFactor...i) {
       var product = i * j
       var productString = String(product)
 
       if String(reverse(productString)) == productString && product > largestProduct {
         largestProduct = product
+        // Continue to next iteration of outer loop - no larger products can be
+        // found within this iteration
+        continue
       }
     }
   }
+
   return largestProduct
 }
 

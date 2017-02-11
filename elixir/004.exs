1e1c2db5dd668ac9aaee5e133809c34388dd94bc
Steve Davis <steve@celery.club>
2017-02-11 18:34:02 -0500

    Fix problem 4 in Elixir to make sure to match the largest product

--- elixir/004.exs
+++ elixir/004.exs
@@ -5,21 +5,26 @@ defmodule PalindromicProduct do
     largest_multiplier = round(:math.pow(10, digits)) - 1
     smallest_multiplier = round(:math.pow(10, digits - 1))
 
-    _largest(smallest_multiplier, largest_multiplier, largest_multiplier)
+    _largest(smallest_multiplier, largest_multiplier, largest_multiplier, 0)
   end
 
-  def _largest(smallest_multiplier, a, b)
+  def _largest(_, a, b, largest_product)
+    when a < 10 and b < 10 do
+    largest_product
+  end
+
+  def _largest(smallest_multiplier, a, b, largest_product)
     when b < smallest_multiplier do
-    _largest(smallest_multiplier, a - 1, a - 1)
+    _largest(smallest_multiplier, a - 1, a - 1, largest_product)
   end
 
-  def _largest(smallest_multiplier, a, b) do
+  def _largest(smallest_multiplier, a, b, largest_product) do
     product_string = Integer.to_string(a * b)
 
     if product_string === String.reverse(product_string) do
-      a * b
+      _largest(smallest_multiplier, a, b - 1, Kernel.max(largest_product, a * b))
     else
-      _largest(smallest_multiplier, a, b - 1)
+      _largest(smallest_multiplier, a, b - 1, largest_product)
     end
   end
 end
